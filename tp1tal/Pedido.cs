e6f2b7b56e864cc62e92a9981c2a889ba8ce9ef9
Santiago Emanuel Castillo <castillosantiagoe1998@gmail.com>
2023-08-27 19:52:45 -0300

    Se creo la funcion de cambio de estado del pedido

--- tp1tal/Pedido.cs
+++ tp1tal/Pedido.cs
@@ -7,9 +7,9 @@ public class Pedido
     {
         public enum Estados {
             Pendiente,
-            Cancelado,
             Encamino,
-            Entregado
+            Entregado,
+            Cancelado,
         }
         private int numero;
 
@@ -22,7 +22,7 @@ public class Pedido
         public Pedido(int numero, string observaciones, string nombre, string direccion, string telefono, string datosRefDireccion){
             this.numero =  numero;
             this.observaciones = observaciones;
-            this.estado = Estados.Pendiente;
+            estado = Estados.Pendiente;
             cliente = new Cliente(nombre, direccion, telefono, datosRefDireccion);
         }
 
@@ -35,16 +35,18 @@ public class Pedido
             Console.WriteLine("Telefono: " + cliente.Telefono);
         } 
 
-        public void cambiarEstado(int cambioEstado){
-            if(cambioEstado == 2){
-
-            }
-            switch (cambioEstado)
-            {
-                case 1:
+        public void cambiarEstado(int tipoCambio){
+            int caso = (int)estado;
+            
+            if(tipoCambio == 1){
+                
+                if(caso == 0){
                     estado = Estados.Encamino;
-                    break;
-                default:
+                }else{
+                    estado = Estados.Entregado;
+                }
+            }else{
+                estado = Estados.Cancelado;
             }
         }
     }
